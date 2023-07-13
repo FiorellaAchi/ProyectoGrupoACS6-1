9c7c29a459196e73eef5c9d65f071b43ce6a54ca
FiorellaAchi <fiorella.achil@ug.edu.ec>
2023-07-12 22:15:03 -0500

    Avance #3

--- ProyectoGrupoA_CS/Facturas/Form1.Designer.cs
+++ ProyectoGrupoA_CS/Facturas/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Facturas
 {
-    partial class Form1
+    partial class FrmCrearFactura
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmCrearFactura
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmCrearFactura";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- ProyectoGrupoA_CS/Facturas/Form1.cs
+++ ProyectoGrupoA_CS/Facturas/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Facturas
 {
-    public partial class Form1 : Form
+    public partial class FrmCrearFactura : Form
     {
-        public Form1()
+        public FrmCrearFactura()
         {
             InitializeComponent();
         }
